3143f9cc4470741bc26d888d26efee046cceee49
siruzhong <1412578784@qq.com>
2023-08-11 14:23:54 +0800

    add demo data

--- caption/llama-adapter-v2.py
+++ caption/llama-adapter-v2.py
@@ -1,31 +1,43 @@
 import asyncio
 import base64
+import glob
 import json
+import os
+from concurrent.futures import ThreadPoolExecutor
+
 import websockets
 from loguru import logger
 
 
 def get_image_base64(image_path):
+    """
+    Encodes the given image as a Base64 string.
+
+    :param image_path: Path to the image file.
+    :return: Base64 encoded string of the image.
+    """
     with open(image_path, 'rb') as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
         return "data:image/jpeg;base64," + encoded_image
 
 
-async def connect():
-    uri = 'ws://llama-adapter.opengvlab.com/queue/join'
-    headers = {}
+async def process_image(image_path):
+    """
+    Processes a single image and returns the description split into sentences.
 
-    async with websockets.connect(uri, extra_headers=headers) as websocket:
-        # 第一条消息
+    :param image_path: Path to the image file.
+    :return: List of description sentences.
+    """
+    uri = 'ws://llama-adapter.opengvlab.com/queue/join'
+    async with websockets.connect(uri) as websocket:
+        # Sending first message to the server
         msg1 = {"fn_index": 1, "session_hash": "4k1lrzequpu"}
         await websocket.send(json.dumps(msg1))
-        response1 = await websocket.recv()
-        logger.info("Response after first message (1): {}", response1)
-        response2 = await websocket.recv()
-        logger.info("Response after first message (2): {}", response2)
+        await websocket.recv()
+        await websocket.recv()
 
-        # 第二条消息
-        image_base64 = get_image_base64('disenzhang.jpg')  # 从本地文件获取图片base64编码
+        # Sending second message containing the image
+        image_base64 = get_image_base64(image_path)
         msg2 = {
             "fn_index": 1,
             "data": [image_base64, "", 128, 0.1, 0.75],
@@ -33,13 +45,62 @@ async def connect():
             "session_hash": "4k1lrzequpu"
         }
         await websocket.send(json.dumps(msg2))
-        response3 = await websocket.recv()
-        logger.info("Response after second message (1): {}", response3)
-        response4 = await websocket.recv()
-        logger.info("Response after second message (2): {}", response4)
-        response5 = await websocket.recv()
-        logger.info("Response after second message (3): {}", response5)
+        await websocket.recv()
+        await websocket.recv()
+        response = await websocket.recv()
+        response_json = json.loads(response)
+        description = response_json["output"]["data"][0]
+
+        # Splitting the description into sentences
+        sentences = description.split('. ')
+        logger.info(f"Processed image: {image_path} - Sentences: {sentences}")
+        return sentences
 
 
-# 启动连接
-asyncio.get_event_loop().run_until_complete(connect())
+def handle_image(image_path):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    return loop.run_until_complete(process_image(image_path))
+
+
+def main():
+    """
+    Main function to process all images in the specified directories and save
+    the captions as JSON objects.
+    """
+    results = []
+    base_directory = 'tiles'
+    cities = ['Beijing', 'Guangzhou', 'Shanghai', 'Shenzhen']
+
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for city in cities:
+            directory_path = os.path.join(base_directory, city)
+            logger.info(f"Processing directory: {directory_path}")
+
+            # Finding all image files in the directory
+            pattern = os.path.join(directory_path, '*.jpg')
+            image_paths = glob.glob(pattern)
+
+            # Processing images concurrently
+            for image_path in image_paths:
+                image_name = os.path.basename(image_path)
+                description_sentences = executor.submit(handle_image, image_path).result()
+
+                # Grouping the JSON for the same image together
+                image_results = []
+                for sentence in description_sentences:
+                    if sentence:  # Avoid empty strings
+                        result = {"caption": sentence.strip(), "image": os.path.join(city, image_name)}
+                        image_results.append(result)
+
+                results.extend(image_results)
+
+    # Saving the results as a JSON file
+    with open('captions.json', 'w') as file:
+        json.dump(results, file)
+    logger.info(f"Saved {len(results)} results to captions.json")
+
+
+# Running the main function
+if __name__ == "__main__":
+    main()
