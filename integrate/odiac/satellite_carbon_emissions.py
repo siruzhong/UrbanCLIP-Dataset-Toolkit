84db0691d2c4ded4869d74240356a21ade37408f
siruzhong <1412578784@qq.com>
2023-09-05 11:03:11 +0800

    enhance csv storage

--- integrate/odiac/satellite_carbon_emissions.py
+++ integrate/odiac/satellite_carbon_emissions.py
@@ -66,40 +66,74 @@ def already_processed_images(csv_filename):
     return processed_images
 
 
-def compute_emission_for_all_images(root_folder, csv_filename):
-    """
-    Computes the carbon emission for all images in the root folder.
-    """
-    processed_images = already_processed_images(csv_filename)
+def get_all_images(root_folder):
+    """Retrieve all image names from the directory."""
+    all_images = set()
+    for city_folder in os.listdir(root_folder):
+        city_path = os.path.join(root_folder, city_folder)
+        if not os.path.isdir(city_path):
+            continue
+        for image_file in os.listdir(city_path):
+            if image_file.endswith('.jpg'):
+                all_images.add(f"{city_folder}/{image_file}")
+    return all_images
 
+
+def csv_file_exists(filename):
+    """Check if a CSV file exists."""
+    return os.path.exists(filename)
+
+
+def create_csv_file(filename):
+    """Create a new CSV file with headers."""
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['satellite_img_name', 'BD09 coordinate', 'WGS84 coordinate', 'carbon_emissions']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+
+def update_csv_with_changes(root_folder, csv_filename):
+    if not csv_file_exists(csv_filename):
+        create_csv_file(csv_filename)
+
+    processed_images = already_processed_images(csv_filename)
+    all_current_images = get_all_images(root_folder)
+
+    # Identify new and removed images
+    new_images = all_current_images - processed_images
+    removed_images = processed_images - all_current_images
+
+    new_data = []
+    for image_key in new_images:
+        city_folder, image_file = image_key.split("/")
+        x, y = extract_coordinates_from_filename(image_file)
+        lat, lon = bd_xy2latlng(16, x, y)
+        emission_value = get_point_carbon_emission(x, y)
+        print(f"Emission for {city_folder}/{image_file}: {emission_value}")
+        new_data.append({
+            'satellite_img_name': image_key,
+            'BD09 coordinate': f"({x * 256 * 2 ** (18 - 16)}, {y * 256 * 2 ** (18 - 16)})",
+            'WGS84 coordinate': f"({lat}, {lon})",
+            'carbon_emissions': emission_value
+        })
+
+    # Process CSV: Load, Remove old entries, Append new data, and Rewrite
+    with open(csv_filename, 'r') as csvfile:
+        reader = list(csv.DictReader(csvfile))
+        # Filtering out removed images
+        reader = [row for row in reader if row['satellite_img_name'] not in removed_images]
+
+    # Append new data and rewrite CSV
     with open(csv_filename, 'w', newline='') as csvfile:
         fieldnames = ['satellite_img_name', 'BD09 coordinate', 'WGS84 coordinate', 'carbon_emissions']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        # Write header only if the file is newly created
-        if not processed_images:
-            writer.writeheader()
-
-        for city_folder in os.listdir(root_folder):
-            city_path = os.path.join(root_folder, city_folder)
-            if not os.path.isdir(city_path):
-                continue
-            for image_file in os.listdir(city_path):
-                if image_file.endswith('.jpg') and f"{city_folder}/{image_file}" not in processed_images:
-                    x, y = extract_coordinates_from_filename(image_file)
-                    lat, lon = bd_xy2latlng(16, x, y)
-                    emission_value = get_point_carbon_emission(x, y)
-                    print(f"Emission for {city_folder}/{image_file}: {emission_value}")
-                    writer.writerow({
-                        'satellite_img_name': f"{city_folder}/{image_file}",
-                        'BD09 coordinate': f"({x * 256 * 2 ** (18 - 16)}, {y * 256 * 2 ** (18 - 16)})",
-                        'WGS84 coordinate': f"({lat}, {lon})",
-                        'carbon_emissions': emission_value
-                    })
+        writer.writeheader()
+        writer.writerows(reader)  # Existing data
+        writer.writerows(new_data)  # New data
 
 
 # Running the main function
 if __name__ == "__main__":
     root_folder = "../../tiles"
-    csv_filename = "emissions.csv"
-    compute_emission_for_all_images(root_folder, csv_filename)
+    csv_filename = "integrated_satellite_data.csv"
+    update_csv_with_changes(root_folder, csv_filename)
