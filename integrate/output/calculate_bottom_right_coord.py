a4d0fb5e325d0f207bd483d495bf93baa8657554
siruzhong <1412578784@qq.com>
2023-11-16 20:09:53 +0800

    update temp_integrated_satellite_data.csv

--- integrate/output/calculate_bottom_right_coord.py
+++ integrate/output/calculate_bottom_right_coord.py
@@ -69,16 +69,27 @@ def calculate_and_add_bottom_right(row):
 
 # Define a threshold for updating the CSV file
 update_threshold = 10  # Update the CSV file every 10 rows
-
 if __name__ == '__main__':
     # Load the CSV file into a pandas DataFrame
     df = pd.read_csv('integrated_satellite_data.csv')
+
+    # Load the existing temporary CSV file (if it exists)
+    try:
+        temp_df = pd.read_csv('temp_integrated_satellite_data.csv')
+    except FileNotFoundError:
+        temp_df = pd.DataFrame()  # Create an empty DataFrame if the file doesn't exist
 
     # Create an empty list to store processed data rows
     processed_rows = []
 
     # Loop through each row and apply the function
     for index, row in df.iterrows():
+        # Check if the temporary DataFrame exists and if the current satellite_img_name is in temp_df
+        if not temp_df.empty and row['satellite_img_name'] in temp_df['satellite_img_name'].values:
+            logger.info(
+                f"Row {index} with satellite_img_name '{row['satellite_img_name']}' already processed in temp file. Skipping...")
+            continue
+
         bottom_right_coord = calculate_and_add_bottom_right(row)
         processed_rows.append(row)  # Append the original row
         processed_rows[-1][
@@ -89,15 +100,18 @@ if __name__ == '__main__':
             # Create a new DataFrame with processed rows
             updated_df = pd.DataFrame(processed_rows)
 
-            # Save the new DataFrame to a temporary CSV file
-            updated_df.to_csv('temp_integrated_satellite_data.csv', index=False)
+            # Append the new DataFrame to the existing temporary CSV file
+            if not temp_df.empty:
+                temp_df = pd.concat([temp_df, updated_df], ignore_index=True)
+            else:
+                temp_df = updated_df
+
+            # Deduplicate based on 'satellite_img_name' column
+            temp_df = temp_df.drop_duplicates(subset='satellite_img_name')
+
+            # Save the updated DataFrame to the temporary CSV file
+            temp_df.to_csv('temp_integrated_satellite_data.csv', index=False)
 
             logger.info(f"Processed {index + 1} rows. Updating CSV file...")
-
-    # After processing all rows, create a final DataFrame with processed rows
-    updated_df = pd.DataFrame(processed_rows)
-
-    # Save the final DataFrame to the CSV file
-    updated_df.to_csv('integrated_satellite_data_with_bottom_right.csv', index=False)
 
     logger.info("CSV file updated with Bottom Right Coordinate.")
