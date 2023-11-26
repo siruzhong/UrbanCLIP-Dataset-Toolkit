78c9f9ed0e668ee5255dc9d6955865ce30d5e50a
siruzhong <1412578784@qq.com>
2023-11-26 12:54:13 +0800

    update v2/pairs

--- v2/img-caption-generator.py
+++ v2/img-caption-generator.py
@@ -12,10 +12,10 @@ from loguru import logger
 
 def get_image_base64(image_path):
     """
-    Encodes the given image as a Base64 string.
+    将给定的图像编码为Base64字符串。
 
-    :param image_path: Path to the image file.
-    :return: Base64 encoded string of the image.
+    :param image_path: 图像文件的路径。
+    :return: 图像的Base64编码字符串。
     """
     with open(image_path, 'rb') as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
@@ -24,11 +24,11 @@ def get_image_base64(image_path):
 
 async def process_image(semaphore, image_path):
     """
-    Processes a single image and returns the description split into sentences.
+    处理单个图像并返回分成句子的描述。
 
-    :param semaphore: Limit the number of tasks running simultaneously
-    :param image_path: Path to the image file.
-    :return: List of description sentences.
+    :param semaphore: 限制同时运行的任务数量
+    :param image_path: 图像文件的路径。
+    :return: 描述句子的列表。
     """
     MAX_RETRIES = 3
     for attempt in range(MAX_RETRIES):
@@ -37,7 +37,7 @@ async def process_image(semaphore, image_path):
                 session_hash = uuid.uuid4().hex
                 uri = 'ws://llama-adapter.opengvlab.com/queue/join'
                 async with websockets.connect(uri) as websocket:
-                    # Sending first message to the server
+                    # 发送第一个消息到服务器
                     msg1 = {"fn_index": 1, "session_hash": session_hash}
                     await websocket.send(json.dumps(msg1))
                     while True:
@@ -46,7 +46,7 @@ async def process_image(semaphore, image_path):
                             logger.info(f"First responses: {session_hash}, {response}")
                             break
 
-                    # Sending second message containing the image
+                    # 发送包含图像的第二个消息
                     image_base64 = get_image_base64(image_path)
                     msg2 = {
                         "fn_index": 1,
@@ -61,15 +61,15 @@ async def process_image(semaphore, image_path):
                             logger.info(f"Second responses: {session_hash}, {response}")
                             break
 
-                    # Splitting the description into sentences
+                    # 将描述拆分成句子
                     description = response["output"]["data"][0]
                     sentences = description.split('. ')
                     logger.info(f"Processed image: {image_path}")
                     return sentences
         except Exception as e:
-            wait_time = random.uniform(1, 3)  # Random wait time between 1 and 3 seconds
+            wait_time = random.uniform(1, 3)  # 随机等待时间在1到3秒之间
             logger.warning(f"Exception on attempt {attempt + 1}: {str(e)}. Retrying...")
-            await asyncio.sleep(wait_time)  # wait a second and try again
+            await asyncio.sleep(wait_time)  # 等待一秒后重试
 
     logger.error(f"Failed to process image after {MAX_RETRIES} attempts: {image_path}")
     return []
@@ -80,28 +80,27 @@ BATCH_SIZE = 5
 
 async def main():
     """
-    Main function to process all images in the specified directories and save
-    the captions as JSON objects.
+    主要函数，用于处理指定目录中的所有图像并将描述保存为JSON对象。
     """
-    # Create 'pairs' directory if it does not exist
+    # 如果不存在，创建'pairs'目录
     output_directory = 'pairs'
     os.makedirs(output_directory, exist_ok=True)
 
-    base_directory = 'stitched_images'
+    base_directory = 'img/masked_images'
     semaphore = asyncio.Semaphore(1000)
 
-    # Finding all image files in the directory
+    # 查找目录中的所有图像文件
     pattern = os.path.join(base_directory, '*.jpg')
     image_paths = glob.glob(pattern)
 
-    # Load existing data if the file exists
+    # 如果文件存在，加载现有数据
     json_filename = os.path.join(output_directory, "captions.json")
     existing_data = []
     if os.path.exists(json_filename):
         with open(json_filename, 'r') as file:
             existing_data = json.load(file)
 
-    # Convert existing data into a set for easy lookup
+    # 将现有数据转换为集合以便于查找
     existing_images = set(item["image"] for sublist in existing_data for item in sublist)
 
     new_image_paths = []
@@ -114,7 +113,7 @@ async def main():
         logger.info(f"All images have already been processed.")
         return
 
-    # Process new images and generate new data
+    # 处理新图像并生成新数据
     new_data = []
     for i in range(0, len(new_image_paths), BATCH_SIZE):
         batch_paths = new_image_paths[i:i + BATCH_SIZE]
@@ -131,13 +130,13 @@ async def main():
         new_data.extend(batch_results)
         logger.info(f"Processed batch {i // BATCH_SIZE + 1}")
 
-        # Flatten existing_data list if it's nested
+        # 如果现有数据列表是嵌套的，将其展平
         existing_data_flat = [item for sublist in existing_data for item in sublist]
 
-        # Combine existing and new data, then write to the JSON file
+        # 合并现有数据和新数据，然后写入JSON文件
         existing_data_flat.extend(new_data)
 
-        # Wrap the entire existing_data_flat list in an additional list
+        # 将整个existing_data_flat列表包装在另一个列表中
         combined_data = [existing_data_flat]
 
         with open(json_filename, 'w', encoding='utf-8') as file:
@@ -145,6 +144,6 @@ async def main():
         logger.info(f"Saved {len(new_data)} results to {json_filename}")
 
 
-# Running the main function
+# 运行主函数
 if __name__ == "__main__":
     asyncio.run(main())
